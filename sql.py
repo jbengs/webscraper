a421d0717f1043528b41ee8756c1a7c372cf0dc7
Joel Bengs <joel.bengs@gmail.com>
2022-06-27 09:29:17 +0200

    checkDatabase works!

--- sql.py
+++ sql.py
@@ -1,88 +1,63 @@
 import sqlite3
+from winreg import QueryInfoKey
 connection = sqlite3.connect("database.db")
 
 
 class sqlClass:
 
-    #def __init__(self):
-     #   sqliteConnection = sqlite3.connect("database.db")
-      #  self.cursor = sqliteConnection.cursor()
+    def __init__(self):
+        self.sqliteConnection = sqlite3.connect("database.db")
+        self.cursor = self.sqliteConnection.cursor()
 
     def createTable(self):
         try:
-            sqliteConnection = sqlite3.connect("database.db")
-            cursor = sqliteConnection.cursor()
-            sqlite_create_table_query = """CREATE TABLE myTable (
+            query= """CREATE TABLE myTable (
                                     id INTEGER PRIMARY KEY,
                                     name TEXT NOT NULL,
                                     email text NOT NULL UNIQUE,
                                     joining_date datetime,
                                     salary REAL NOT NULL);"""
-            cursor.execute(sqlite_create_table_query)
-            sqliteConnection.commit()
+            self.cursor.execute(query)
+            self.sqliteConnection.commit()
             print("SQLite table created")
-            cursor.close()
         except sqlite3.Error as error:
             print("Failed to create sqlite table", error)
-        finally:
-            if sqliteConnection:
-                sqliteConnection.close()
 
     def insertVaribleIntoTable(self, id, name, email, joinDate, salary):
         try:
-            sqliteConnection = sqlite3.connect('database.db')
-            cursor = sqliteConnection.cursor()
             sqlite_insert_with_param = """INSERT INTO myTable
                             (id, name, email, joining_date, salary) 
                             VALUES (?, ?, ?, ?, ?);"""
             data_tuple = (id, name, email, joinDate, salary)
-            cursor.execute(sqlite_insert_with_param, data_tuple)
-            sqliteConnection.commit()
+            self.cursor.execute(sqlite_insert_with_param, data_tuple)
+            self.sqliteConnection.commit()
             print("Python Variables inserted successfully into myTable table")
-            cursor.close()
         except sqlite3.Error as error:
             print("Failed to insert Python variable into sqlite table", error)
-        finally:
-            if sqliteConnection:
-                sqliteConnection.close()
 
+    # Checks wheter the row allready exist in the database. Returns True if it exists, else False
     def checkDatabase(self, id):
         try:
-            sqliteConnection = sqlite3.connect('database.db')
-            cursor = sqliteConnection.cursor()
-            sqlite_select_with_param = "SELECT id FROM myTable WHERE id = 20;"
-            #data_tuple = (id,)
-            rows = cursor.execute(sqlite_select_with_param)
-            sqliteConnection.commit()
-            print("Python Variables checked from myTable table")
-            
-            if rows:
-                cursor.close()
-                print("Returned True")
+            query = "SELECT id FROM myTable WHERE id = ?"
+            data_tuple = (id,)
+            self.cursor.execute(query, data_tuple)
+            rows = self.cursor.fetchone()
+            self.sqliteConnection.commit()
+            if not rows is None:
+                print(f'id {id} exist in myTable: True')
                 return True
             else:
-                print ("returned False")
-                cursor.close()
+                print(f'id {id} exist in myTable: False')
                 return False
         except sqlite3.Error as error:
-            print("Failed to check Python variable into sqlite table", error)
-        finally:
-            if sqliteConnection:
-                sqliteConnection.close()
+            print("Failed to check sqlite table. Error: ", error)
 
     def fetchDatabase(self):
         try:
-            sqliteConnection = sqlite3.connect('database.db')
-            cursor = sqliteConnection.cursor()
-            cursor.execute("SELECT * FROM myTable")
-            rows = cursor.fetchall()
+            self.cursor.execute("SELECT * FROM myTable")
+            rows = self.cursor.fetchall()
             for row in rows:
                 print(row)
-            sqliteConnection.commit()
-            print("Python Variables checked from myTable table")
-            cursor.close()
+            self.sqliteConnection.commit()
         except sqlite3.Error as error:
             print("Failed to fetchAll from myTable: ", error)
-        finally:
-            if sqliteConnection:
-                sqliteConnection.close()
