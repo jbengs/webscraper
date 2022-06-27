35604723b821c9381c617d76c205f6644204684c
Joel Bengs <joel.bengs@gmail.com>
2022-06-27 09:00:11 +0200

    before creating a constructor for sql class

--- sql.py
+++ sql.py
@@ -1,18 +1,23 @@
 import sqlite3
 connection = sqlite3.connect("database.db")
 
+
 class sqlClass:
+
+    #def __init__(self):
+     #   sqliteConnection = sqlite3.connect("database.db")
+      #  self.cursor = sqliteConnection.cursor()
+
     def createTable(self):
         try:
             sqliteConnection = sqlite3.connect("database.db")
             cursor = sqliteConnection.cursor()
-            print("Connected to SQLite")
-            sqlite_create_table_query = '''CREATE TABLE SqliteDb_developers (
+            sqlite_create_table_query = """CREATE TABLE myTable (
                                     id INTEGER PRIMARY KEY,
                                     name TEXT NOT NULL,
                                     email text NOT NULL UNIQUE,
                                     joining_date datetime,
-                                    salary REAL NOT NULL);'''
+                                    salary REAL NOT NULL);"""
             cursor.execute(sqlite_create_table_query)
             sqliteConnection.commit()
             print("SQLite table created")
@@ -27,20 +32,57 @@ class sqlClass:
         try:
             sqliteConnection = sqlite3.connect('database.db')
             cursor = sqliteConnection.cursor()
-            print("Connected to SQLite")
-            sqlite_insert_with_param = """INSERT INTO SqliteDb_developers
+            sqlite_insert_with_param = """INSERT INTO myTable
                             (id, name, email, joining_date, salary) 
                             VALUES (?, ?, ?, ?, ?);"""
-
             data_tuple = (id, name, email, joinDate, salary)
             cursor.execute(sqlite_insert_with_param, data_tuple)
             sqliteConnection.commit()
-            print("Python Variables inserted successfully into SqliteDb_developers table")
+            print("Python Variables inserted successfully into myTable table")
             cursor.close()
         except sqlite3.Error as error:
             print("Failed to insert Python variable into sqlite table", error)
         finally:
             if sqliteConnection:
                 sqliteConnection.close()
-                print("The SQLite connection is closed")
 
+    def checkDatabase(self, id):
+        try:
+            sqliteConnection = sqlite3.connect('database.db')
+            cursor = sqliteConnection.cursor()
+            sqlite_select_with_param = "SELECT id FROM myTable WHERE id = 20;"
+            #data_tuple = (id,)
+            rows = cursor.execute(sqlite_select_with_param)
+            sqliteConnection.commit()
+            print("Python Variables checked from myTable table")
+            
+            if rows:
+                cursor.close()
+                print("Returned True")
+                return True
+            else:
+                print ("returned False")
+                cursor.close()
+                return False
+        except sqlite3.Error as error:
+            print("Failed to check Python variable into sqlite table", error)
+        finally:
+            if sqliteConnection:
+                sqliteConnection.close()
+
+    def fetchDatabase(self):
+        try:
+            sqliteConnection = sqlite3.connect('database.db')
+            cursor = sqliteConnection.cursor()
+            cursor.execute("SELECT * FROM myTable")
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+            sqliteConnection.commit()
+            print("Python Variables checked from myTable table")
+            cursor.close()
+        except sqlite3.Error as error:
+            print("Failed to fetchAll from myTable: ", error)
+        finally:
+            if sqliteConnection:
+                sqliteConnection.close()
--- sqlMain.py
+++ sqlMain.py
@@ -1,7 +1,13 @@
 from sql import sqlClass
 
 obj = sqlClass()
-obj.createTable
+obj.createTable()
 obj.insertVaribleIntoTable(2, 'Joe', 'joe@pynative.com', '2019-05-19', 9000)
-obj.insertVaribleIntoTable(3, 'Ben', 'ben@pynative.com', '2019-02-23', 9500)    
+obj.insertVaribleIntoTable(3, 'Ben', 'ben@pynative.com', '2019-02-23', 9500)
+obj.fetchDatabase()
+if obj.checkDatabase(2):
+    print("Recieved True on id = 2")
+if obj.checkDatabase(10):
+    print("Recieved True on id = 10")
+
 
